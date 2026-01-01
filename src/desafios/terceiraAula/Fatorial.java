01c366cfd6b755a8a3478b4d906d5f1817e5219a
Maria Cláudia <mariaclaudiapio@gmail.com>
2026-01-01 11:43:02 -0300

    inclusão do exercício sobre fatorial

---
+++ src/desafios/terceiraAula/Fatorial.java
@@ -0,0 +1,23 @@
+// Crie um programa que solicite um número ao usuário e que calcule o fatorial desse número
+
+package desafios.terceiraAula;
+
+import java.util.Scanner;
+
+public class Fatorial
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        int fatorial = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println("O fatorial de " + numero + " é: " + fatorial);
+    }
+}
