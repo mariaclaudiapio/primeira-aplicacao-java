de6c1d9fea34b553fd5ff538ae9eded53f48bfb5
Maria Cláudia <mariaclaudiapio@gmail.com>
2025-12-31 22:51:36 -0300

    tabuada de adição

--- src/desafios/terceiraAula/Tabuada.java
+++ src/desafios/terceiraAula/Tabuada.java
@@ -1,9 +1,25 @@
+/* Crie um programa que solicite ao usuário um número e exiba a tabuada desse número de
+1 a 10 */
+
 package desafios.terceiraAula;
+
+import java.util.Scanner;
 
 public class Tabuada
 {
     public static void main(String[] args)
     {
         System.out.println("Informe um número inteiro:");
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = scanner.nextInt();
+
+        System.out.printf("Tabuada de adição do número %d:\n", numero);
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println(numero + " + " + i + " = " + (numero + i));
+        }
+
+        scanner.close();
     }
 }
